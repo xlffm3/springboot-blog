164cb349b95c2928ecbab00b95aa83c652cb438f
Jinhong <xlffm3@gmail.com>
2021-08-19 16:36:41 +0900

    feat: queryDSL 및 jpa auditing configuration 설정 (#9)

---
+++ src/main/java/com/spring/blog/configuration/JpaConfiguration.java
@@ -0,0 +1,9 @@
+package com.spring.blog.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfiguration {
+}
