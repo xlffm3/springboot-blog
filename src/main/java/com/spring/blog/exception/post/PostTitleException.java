62c29d6c5b1bd1b7a1010e8783154c93699d2c7d
Jinhong <xlffm3@gmail.com>
2021-08-19 16:21:28 +0900

    refactor: 불필요한 Post 계층 기능 삭제 및 10000자 검증 추가 (#6)

--- src/main/java/com/spring/blog/exception/post/PostTitleException.java
+++ src/main/java/com/spring/blog/exception/post/PostTitleException.java
@@ -2,17 +2,17 @@ package com.spring.blog.exception.post;
 
 import org.springframework.http.HttpStatus;
 
-public class InvalidTitleException extends PostException {
+public class PostTitleException extends PostException {
 
     private static final String INVALID_TITLE_MESSAGE =
         "게시글 제목은 공백이거나 100자를 초과할 수 없습니다.";
     private static final String INVALID_TITLE_ERROR_CODE = "P0001";
 
-    public InvalidTitleException() {
+    public PostTitleException() {
         this(INVALID_TITLE_MESSAGE, INVALID_TITLE_ERROR_CODE, HttpStatus.BAD_REQUEST);
     }
 
-    public InvalidTitleException(String message, String errorCode, HttpStatus httpStatus) {
+    public PostTitleException(String message, String errorCode, HttpStatus httpStatus) {
         super(message, errorCode, httpStatus);
     }
 }
