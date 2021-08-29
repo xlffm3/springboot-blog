8dcfb35d0393b75e9f150d15359a5bdb2d9641d9
Jinhong <xlffm3@gmail.com>
2021-08-29 17:05:59 +0900

    feat: Post 삭제 API 구현 (#19)

--- backend/src/test/java/com/spring/blog/post/presentation/PostControllerTest.java
+++ backend/src/test/java/com/spring/blog/post/presentation/PostControllerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -19,6 +20,7 @@ import com.spring.blog.authentication.domain.user.LoginUser;
 import com.spring.blog.common.FileFactory;
 import com.spring.blog.common.PageMaker;
 import com.spring.blog.post.application.PostService;
+import com.spring.blog.post.application.dto.PostDeleteRequestDto;
 import com.spring.blog.post.application.dto.PostListRequestDto;
 import com.spring.blog.post.application.dto.PostListResponseDto;
 import com.spring.blog.post.application.dto.PostResponseDto;
@@ -31,6 +33,7 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -65,6 +68,8 @@ class PostControllerTest {
         mockMvc.perform(multipart("/api/posts"))
             .andExpect(status().isUnauthorized())
             .andExpect(jsonPath("$.errorCode").value("A0001"));
+
+        verify(oAuthService, times(1)).validateToken(any());
     }
 
     @DisplayName("로그인 유저는 게시물을 작성할 수 있다.")
@@ -74,7 +79,8 @@ class PostControllerTest {
         String token = "Bearer token";
         AppUser appUser = new LoginUser(1L, "kevin");
         PostResponseDto postResponseDto =
-            new PostResponseDto(1L, "title", "content", Arrays.asList("url1", "url2"), "kevin", 0L, null, null);
+            new PostResponseDto(1L, "title", "content", Arrays.asList("url1", "url2"), "kevin", 0L,
+                null, null);
 
         given(oAuthService.validateToken("token")).willReturn(true);
         given(oAuthService.findRequestUserByToken("token")).willReturn(appUser);
@@ -103,7 +109,8 @@ class PostControllerTest {
     void read_SinglePost_Success() throws Exception {
         // given
         PostResponseDto postResponseDto =
-            new PostResponseDto(1L, "title", "content", Arrays.asList("url1", "url2"), "kevin", 1L, null, null);
+            new PostResponseDto(1L, "title", "content", Arrays.asList("url1", "url2"), "kevin", 1L,
+                null, null);
         PostResponse postResponse = PostResponse.from(postResponseDto);
 
         given(postService.readById(1L)).willReturn(postResponseDto);
@@ -135,11 +142,46 @@ class PostControllerTest {
             .willReturn(postListResponseDto);
 
         // when
-        mockMvc.perform(get("/api/posts?page={page}&size={size}&pageBlockCounts={block}", "0", "3", "5"))
+        mockMvc.perform(
+            get("/api/posts?page={page}&size={size}&pageBlockCounts={block}", "0", "3", "5"))
             .andExpect(status().isOk())
             .andExpect(content().string(expectedResponseBody));
 
         // then
         verify(postService, times(1)).readPostList(any(PostListRequestDto.class));
     }
+
+    @DisplayName("비로그인 유저는 게시물 삭제가 불가능하다.")
+    @Test
+    void delete_GuestUser_ExceptionThrown() throws Exception {
+        // given
+        given(oAuthService.validateToken(any())).willReturn(false);
+
+        // when, then
+        mockMvc.perform(delete("/api/posts/1"))
+            .andExpect(status().isUnauthorized())
+            .andExpect(jsonPath("$.errorCode").value("A0001"));
+
+        verify(oAuthService, times(1)).validateToken(any());
+    }
+
+    @DisplayName("로그인 유저는 게시물 삭제가 가능하다.")
+    @Test
+    void delete_LoginUser_ExceptionThrown() throws Exception {
+        // given
+        given(oAuthService.validateToken("token")).willReturn(true);
+        given(oAuthService.findRequestUserByToken("token"))
+            .willReturn(new LoginUser(1L, "kevin"));
+        Mockito.doNothing().when(postService).deletePost(any(PostDeleteRequestDto.class));
+
+        // when, then
+        mockMvc.perform(delete("/api/posts/1")
+            .header(HttpHeaders.AUTHORIZATION, "Bearer token"))
+            .andExpect(status().isNoContent());
+
+        verify(oAuthService, times(1)).validateToken("token");
+        verify(oAuthService, times(1)).findRequestUserByToken("token");
+        verify(postService, times(1)).deletePost(any(PostDeleteRequestDto.class));
+    }
 }
+
