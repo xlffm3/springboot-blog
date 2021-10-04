3871f41e5339d72f64658f5a607bec7b6da87eeb
Jinhong <xlffm3@gmail.com>
2021-10-04 15:54:42 +0900

    chore: UserRegistration Pattern 삭제

--- backend/src/main/java/com/spring/blog/user/presentation/dto/UserRegistrationRequest.java
+++ backend/src/main/java/com/spring/blog/user/presentation/dto/UserRegistrationRequest.java
@@ -2,7 +2,6 @@ package com.spring.blog.user.presentation.dto;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +15,6 @@ import lombok.NoArgsConstructor;
 public class UserRegistrationRequest {
 
     @NotBlank
-    @Pattern(regexp = "[a-zA-Z가-힣]{2,10}")
     private String name;
 
     @NotBlank
